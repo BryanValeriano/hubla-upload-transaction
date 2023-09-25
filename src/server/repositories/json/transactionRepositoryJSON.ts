817a0e29f720feb8a610dc25e93d726a0a5b6975
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-09-25 17:06:06 -0300

    create JSON transactionRespository

---
+++ src/server/repositories/json/transactionRepositoryJSON.ts
@@ -0,0 +1,21 @@
+import Transaction from "@/server/entities/Transaction";
+import ITransactionRepository from "../ITransactionRepository";
+import fs from "fs";
+import path from "node:path";
+const databasePath = path.join(__dirname, "./transactions.json");
+
+class TransactionRepositoryJSON implements ITransactionRepository {
+  async insert(transaction: Transaction): Promise<void> {
+    const transactions = this.getTransactionsFromFile();
+    transactions.push(transaction);
+  }
+
+  async getAll(): Promise<Transaction[]> {
+    return [...this.getTransactionsFromFile()];
+  }
+
+  private getTransactionsFromFile(): Transaction[] {
+    const transactions: Transaction[] = JSON.parse(fs.readFileSync(databasePath, "utf8"));
+    return transactions.map((transaction) => new Transaction({ ...transaction }));
+  }
+}
