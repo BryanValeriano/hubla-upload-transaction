d56bb9e49c628619101c8e2ca9b15468c6743983
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-09-26 14:39:15 -0300

    perf better style on TransactionList component

--- src/app/components/TransactionList.tsx
+++ src/app/components/TransactionList.tsx
@@ -1,24 +1,39 @@
 import Transaction from "@/server/entities/Transaction";
 
 interface TransactionProps {
-  transactions: Omit<Transaction, "id">[]; // Substitua por seu tipo de transação real
+  transactions: Omit<Transaction, "id">[];
 }
 
 export default function TransactionList({ transactions }: TransactionProps) {
   return (
     <div className="mt-4">
-      <h2 className="text-lg font-semibold">Transações:</h2>
-      <ul>
+      <h2 className="text-2xl font-semibold mb-4">Transações:</h2>
+      <div className="space-y-4">
         {transactions.map((transaction, index) => (
-          <li key={index}>
-            {transaction.type}
-            {transaction.date}
-            {transaction.productDescription}
-            {transaction.value}
-            {transaction.transactionOwnerName}
-          </li>
+          <div key={index} className="p-4 border rounded shadow-sm">
+            <div className="border-b border-gray-200 pb-2 mb-2">
+              <span className="text-gray-500">Tipo:</span>
+              <span className="font-medium ml-2">{transaction.type}</span>
+            </div>
+            <div className="border-b border-gray-200 pb-2 mb-2">
+              <span className="text-gray-500">Data:</span>
+              <span className="font-medium ml-2">{transaction.date}</span>
+            </div>
+            <div className="border-b border-gray-200 pb-2 mb-2">
+              <span className="text-gray-500">Produto:</span>
+              <span className="font-medium ml-2">{transaction.productDescription}</span>
+            </div>
+            <div className="border-b border-gray-200 pb-2 mb-2">
+              <span className="text-gray-500">Valor:</span>
+              <span className="font-medium ml-2">{transaction.value}</span>
+            </div>
+            <div className="pb-2">
+              <span className="text-gray-500">Vendedor:</span>
+              <span className="font-medium ml-2">{transaction.transactionOwnerName}</span>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
