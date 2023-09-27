e4026ecb3461e9b8aaf20d99ddd71ca51aed9850
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-09-27 11:46:52 -0300

    remove debug

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
     const { transactionRepository } = container();
     const getAllTransactionsController = new GetAllTransactionsController({ transactionRepository });
     const transactions = await getAllTransactionsController.execute();
-    return NextResponse.json({ success: true, transactions, teste: "saudacoes" }, { status: 200 });
+    return NextResponse.json({ success: true, transactions }, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ success: false, error: 'Internal Server Error' }, { status: 500 });
