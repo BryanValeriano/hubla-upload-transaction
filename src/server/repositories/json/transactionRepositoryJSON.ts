efddd4284bec7728a493f568eb64d80bc4eefecd
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-09-27 00:06:12 -0300

    fix JSON repository

--- src/server/repositories/json/transactionRepositoryJSON.ts
+++ src/server/repositories/json/transactionRepositoryJSON.ts
@@ -2,12 +2,35 @@ import Transaction from "@/server/entities/Transaction";
 import ITransactionRepository from "../ITransactionRepository";
 import fs from "fs";
 import path from "node:path";
-const databasePath = path.join(__dirname, "./transactions.json");
 
 export default class TransactionRepositoryJSON implements ITransactionRepository {
+  private filePath = path.join(
+    'src',
+    'server',
+    'repositories',
+    'json',
+    'files',
+    "transactions.json"
+  );
+
+  constructor() {
+    fs.readFile(this.filePath, (err) => {
+      console.error("Error on loading JSON database")
+    })
+  }
+
+  private async saveToFile(transactions: Transaction[]) {
+    fs.writeFileSync(this.filePath, JSON.stringify(transactions));
+  }
+
+  async clear(): Promise<void> {
+    await this.saveToFile([])
+  }
+
   async insert(transaction: Transaction): Promise<void> {
     const transactions = this.getTransactionsFromFile();
     transactions.push(transaction);
+    this.saveToFile(transactions);
   }
 
   async getAll(): Promise<Transaction[]> {
@@ -15,7 +38,10 @@ export default class TransactionRepositoryJSON implements ITransactionRepository
   }
 
   private getTransactionsFromFile(): Transaction[] {
-    const transactions: Transaction[] = JSON.parse(fs.readFileSync(databasePath, "utf8"));
+    const transactions: Transaction[] = JSON.parse(fs.readFileSync(
+      this.filePath,
+      'utf8',
+    ));
     return transactions.map((transaction) => new Transaction({ ...transaction }));
   }
 }
