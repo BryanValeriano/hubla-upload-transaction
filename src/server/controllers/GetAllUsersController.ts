53574b05e38e1e105b5986f85a245ebeb5dfd8be
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-09-28 13:02:49 -0300

    add GetAllUsersController

---
+++ src/server/controllers/GetAllUsersController.ts
@@ -0,0 +1,23 @@
+import User from "../entities/User";
+import IUserRepository from "../repositories/IUserRepository";
+import GetUsersService from "../useCases/getUsers/getUsersService";
+
+type GetAllUsersControllerProps = {
+  userRepository: IUserRepository;
+}
+export default class GetAllUsersController {
+  private userRepository: IUserRepository;
+  private getUsersService: GetUsersService;
+
+  constructor({ userRepository }: GetAllUsersControllerProps) {
+    this.userRepository = userRepository;
+    this.getUsersService = new GetUsersService({
+      userRepository: this.userRepository
+    });
+
+  }
+
+  public async execute(): Promise<User[]> {
+    return await this.getUsersService.execute();
+  }
+}
