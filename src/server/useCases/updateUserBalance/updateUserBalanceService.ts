2564dfb33a27139d80c92655cea4463b6a533fa0
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-09-27 21:11:32 -0300

    add updateUserBalanceService

---
+++ src/server/useCases/updateUserBalance/updateUserBalanceService.ts
@@ -0,0 +1,26 @@
+import User from "@/server/entities/User";
+import IUserRepository from "@/server/repositories/IUserRepository";
+
+type Dependencies = {
+  userRepository: IUserRepository;
+}
+
+export default class UpdateUserBalanceService {
+  private userRepository: IUserRepository;
+
+  constructor({ userRepository }: Dependencies) {
+    this.userRepository = userRepository;
+  }
+
+  async execute(user: User): Promise<User | undefined> {
+    const existingUser = await this.userRepository.getByName(user.userName)
+
+    if (!existingUser) {
+      console.error("Error on updatng non existing user");
+      return;
+    }
+
+    await this.userRepository.updateBalance(user);
+    return user;
+  }
+}
