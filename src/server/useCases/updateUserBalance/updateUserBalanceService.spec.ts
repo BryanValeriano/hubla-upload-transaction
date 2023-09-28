329fdf06d3dca4c08f49e3becf62b96372988120
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-09-27 21:30:33 -0300

    add test for updateUserBalanceService

---
+++ src/server/useCases/updateUserBalance/updateUserBalanceService.spec.ts
@@ -0,0 +1,38 @@
+import UserRepositoryInMemory from "@/server/repositories/in-memory/userRepositoryInMemory"
+import { describe, expect, it } from "vitest";
+import CreateUserService from "../createUser/createUserService";
+import UpdateUserBalanceService from "./updateUserBalanceService";
+import GetUsersService from "../getUserByName/getUserByNameService";
+
+describe("Update User Balance Service", () => {
+  it("Should be able to update existing user balance", async () => {
+    const userRepository = new UserRepositoryInMemory();
+
+    const createUserService = new CreateUserService({
+      userRepository
+    });
+    const updateUserBalanceService = new UpdateUserBalanceService({
+      userRepository
+    });
+    const getUserByNameService = new GetUsersService({
+      userRepository
+    });
+
+    const userName = "teste"
+    const balance = Math.random();
+
+    await createUserService.execute({
+      userName: userName
+    })
+    await updateUserBalanceService.execute({
+      userName,
+      balance,
+    })
+
+    const user = await getUserByNameService.execute(userName);
+
+    expect(user).not.toBe(undefined);
+    expect(user?.userName).toBe(userName);
+    expect(user?.balance).toBe(balance);
+  })
+})
